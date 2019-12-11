3b36355d06606298c75e835a0ede5fceafa55022
mohamedfaridelsaid <mohammed_eliraqi@yahoo.com>
2019-12-11 17:27:15 +0200

    some thing

---
+++ todo/templates/static/javascript/jscript.js
@@ -0,0 +1,13 @@
+function set_background_color_red() {
+  document.body.style.backgroundColor = "red";
+}
+function set_background_color_green() {
+  document.body.style.backgroundColor = "green";
+}
+function set_background_color_blue() {
+  document.body.style.backgroundColor = "blue";
+}
+
+window.addEventListener("load", function() {
+  set_background_color_red();
+});
