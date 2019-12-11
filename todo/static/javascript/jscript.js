d0da3671de387ec87e0fbbc4a1995dbd38a422b2
mohamedfaridelsaid <mohammed_eliraqi@yahoo.com>
2019-12-11 17:24:44 +0200

    edit gitignore

--- todo/static/javascript/jscript.js
+++
@@ -1,6 +0,0 @@
-function change_background_color(){
-    random_num =Math.floor(Math.random()*5)
-    color_list =["#E91E63","#009688","#607D8B","#4FC3F7","#880E4F"]
-
-    document.body.style.backgroundColor = color_list[random_num];
-}
