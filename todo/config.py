0a1e5ec9a3a0d2ee67ff5f7c29512ffc7504721c
mohamedfaridelsaid <mohammed_eliraqi@yahoo.com>
2020-01-26 23:14:14 +0200

    add some styling for missions

--- todo/config.py
+++ todo/config.py
@@ -3,4 +3,4 @@ dbpath = os.path.join(os.path.abspath(os.curdir),"todo/todo.db")
 SECRET_KEY = "secert"
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(dbpath)
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-POSTS_PER_PAGE=5
+POSTS_PER_PAGE=10
