3ae32ef57564817bb4c96f98aac78d0e26efad71
mohamedfaridelsaid <mohammed_eliraqi@yahoo.com>
2020-01-26 23:38:50 +0200

    add email logger

--- todo/__init__.py
+++ todo/__init__.py
@@ -3,6 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flask_login import LoginManager
 from flask_moment import Moment
+import logging
+from logging.handlers import SMTPHandler
+
+
+
+
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -10,20 +16,41 @@ login = LoginManager()
 moment = Moment()
 def create_app(configfile="config.py"):
     app = Flask(__name__)
+    app.config.from_pyfile(configfile)
+    
     from .missions.routes import mission
     from .users.routes import user
     from .errors import handler
     from todo.missions import models
-    app.config.from_pyfile(configfile)
+    
     db.init_app(app)
     moment.init_app(app)
     login.init_app(app)
     login.login = 'user.login'
     migrate.init_app(app, db)
 
-
     app.register_blueprint(mission)
     app.register_blueprint(user)
-    app.register_blueprint(handler.error)  
-
+    app.register_blueprint(handler.error)
+    handle_email_log(app)
     return app
+
+def handle_email_log(app):
+    if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+    secure = None
+    if app.config['MAIL_USE_TLS']:
+        secure = ()
+    mail_handler = SMTPHandler(
+        mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+        fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+        toaddrs=app.config['ADMINS'], subject=' Todo Failure',
+        credentials=auth, secure=secure
+        )
+    
+    mail_handler.setLevel(logging.ERROR)
+    mail_handler.setFormatter(logging.Formatter(
+        '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
+    ))
+    if not app.debug:
+        app.logger.addHandler(mail_handler)
--- todo/config.py
+++ todo/config.py
@@ -3,4 +3,12 @@ dbpath = os.path.join(os.path.abspath(os.curdir),"todo/todo.db")
 SECRET_KEY = "secert"
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(dbpath)
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-POSTS_PER_PAGE=10
+POSTS_PER_PAGE = 10
+MAIL_SERVER = os.environ.get('MAIL_SERVER')
+MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+ADMINS = ['mhmdmedo820@gmail.com']
+POSTS_PER_PAGE = 25
+
